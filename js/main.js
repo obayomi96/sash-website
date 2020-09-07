75f7b88dc70587317da8780b2f1572354c54d201
Martins Obayomi <obayomimartins96@gmail.com>
2020-09-07 04:16:35 +0100

    Ft: submit btn

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 const modalbtn = document.querySelector('.btn-submit');
+const modalbtn2 = document.querySelector('.btn-submit2');
 const closeModalbtn = document.querySelector('.close-modal');
 const modalDiv = document.querySelector('.modal-div');
-const modalCard = document.querySelector('.modal-card');
-const emailInput = document.querySelector('.email-input');
+// const modalCard = document.querySelector('.modal-card');
+// const emailInput = document.querySelector('.email-input');
 
 let showModal = false;
 
-modalbtn.addEventListener('click', (e) => {
+const modalFunc = (e) => {
   e.preventDefault();
   showModal = true;
   if(showModal) {
     modalDiv.style.display = 'block';
   } else {
+    showModal = false;
     modalDiv.style.display = 'none';
   }
-});
+  showModal = !showModal;
+}
 
-closeModalbtn.addEventListener('click', () => {
+modalbtn.addEventListener('click', (e) => modalFunc(e));
+
+modalbtn2.addEventListener('click', (e) => modalFunc(e));
+
+closeModalbtn.addEventListener('click', (e) => {
   modalDiv.style.display = 'none';
 });
